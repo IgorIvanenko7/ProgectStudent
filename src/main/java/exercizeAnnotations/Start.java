e69325edf3ef548db35fd3eaa98351217388a62f
Igor <igoronigor777@yandex.ru>
2024-04-01 01:45:50 +0300

    add Dev

--- src/main/java/exercizeAnnotations/Start.java
+++ src/main/java/exercizeAnnotations/Start.java
@@ -3,6 +3,7 @@ package exercizeAnnotations;
 public class Start {
     public static void main(String[] args) throws InstantiationException,
                                                   IllegalAccessException {
+
         TestRunner.runTests(ClassForTests.class);
         System.out.println("# End tests #");
     }
