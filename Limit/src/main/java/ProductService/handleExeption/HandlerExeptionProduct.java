7dc8bab87b1d7b98b4ba9d7c088eb9cad8c829f6
Igor <igoronigor777@yandex.ru>
2024-06-03 09:11:47 +0300

    update Limit Projects

--- Limit/src/main/java/ProductService/handleExeption/HandlerExeptionProduct.java
+++
@@ -1,7 +0,0 @@
-package ProductService.handleExeption;
-
-public class HandlerExeptionProduct extends RuntimeException {
-    public HandlerExeptionProduct(String messException, String anyValue) {
-        super(String.join(" ", messException,  anyValue));
-    }
-}
--- SQL/schema.sql
+++ SQL/schema.sql
@@ -1,34 +1,40 @@
---//-- Create users table
+drop table if exists limits;
+drop table if exists payments;
 drop table if exists users;
+
+--//-- Создание таблицы Пользователей(клиентов)
 create table users (
    id serial primary key,
    username varchar (255) not null unique
 );
 
---//-- Create payment table
-drop table if exists payments;
+--//-- Создание таблицы Платежей
 create table payments (
    id serial primary key,
    idUser int not null references users on delete cascade,
-   sumPay numeric null,
-   datePay timestamp
+   sumPay numeric not null,
+   datePay timestamp not null unique
 );
 
---//-- Create limit table
-drop table if exists limits;
+--//-- Создание таблицы Лимитов
 create table limits (
    id serial primary key,
    idUser int not null references users on delete cascade,
-   sumlimit numeric null,
-   dateinstall timestamp
+   sumDaylimit numeric null,
+   dateinstall timestamp,
+   sumBaselimit numeric null
 );
 
---//-- insert example Data
-insert into users(username) values ('User1'), ('User2'), ('User3');
+--//-- Добавление пользователей(клиентов - 100)
+insert into users(username)
+select 'User' || f :: text
+from generate_series(1, 100) f;
 
-insert into limits(idUser, sumlimit, dateinstall)
-	values ((select id from users where username = 'User1'), 10000.00, current_timestamp);
-insert into limits(idUser, sumlimit, dateinstall)
-	values ((select id from users where username = 'User2'), 10000.00, current_timestamp);
-insert into limits(idUser, sumlimit, dateinstall)
-	values ((select id from users where username = 'User3'), 10000.00, current_timestamp);
+--//-- Добавление некоторых лимитов.
+--//-- Остальные, при отсутствии будут добавлены автоматически по требованию
+insert into limits(idUser, sumDaylimit, dateinstall, sumBaselimit)
+	values ((select id from users where username = 'User1'), 10000.00, current_timestamp, 10000.00);
+insert into limits(idUser, sumDaylimit, dateinstall, sumBaselimit)
+	values ((select id from users where username = 'User2'), 10000.00, current_timestamp, 10000.00);
+insert into limits(idUser, sumDaylimit, dateinstall, sumBaselimit)
+	values ((select id from users where username = 'User3'), 10000.00, current_timestamp, 10000.00);
